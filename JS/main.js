1031617f842355cabb5b5441d5cc4a5e87a98bfc
Kartik Malhotra <kartik@kartiks-mbp-2.home>
2019-08-24 20:05:16 +0100

    Added new JS file to handle global logic.

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    // Call the lightbox pluglin 
+    $('.gallery a').simpleLightbox();
+
+    // Call the autoslider method for banner images
+    autoSlides();
+});
+
--- JS/slider.js
+++ JS/slider.js
@@ -28,7 +28,3 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
 }
-
-$(document).ready(function(){
-  //autoSlides();
-});
