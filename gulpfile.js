f869abb4abcbf65c4bf1251a754f46887db25c71
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-04-11 12:08:04 +0530

    No comments

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 
 exports.default = () => (
-	gulp.src('./Images/favicon_io/*')
+	gulp.src('./Images/AboutUs.jpg')
 		.pipe(imagemin())
-        .pipe(gulp.dest('Gallery/Icons/favicon_io'))
+        .pipe(gulp.dest('Gallery'))
 );
+
+// exports.default = () => (
+// 	gulp.src('./Images/favicon_io/*')
+// 		.pipe(imagemin())
+//         .pipe(gulp.dest('Gallery/Icons/favicon_io'))
+// );
