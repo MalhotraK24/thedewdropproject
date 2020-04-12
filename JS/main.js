9b4d3bdad22c4829abc0309b68494182334b4c7c
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-04-13 01:57:37 +0530

    Updated main.js to uncomment some code

--- JS/main.js
+++ JS/main.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
     // Display success message on form submission
     $("#my-form").submit(function(e) {
         e.preventDefault();
-        showAlert();
-        //var $form = $(this);
-        // $.post($form.attr("action"), $form.serialize()).then(function() {
-        //   $('.success-message').show();
-        // });
-        //$form[0].reset();
+        //showAlert();
+        var $form = $(this);
+        $.post($form.attr("action"), $form.serialize()).then(function() {
+          showAlert();
+        });
+        $form[0].reset();
         return false;
       });
 });
