353680338b394c63e32f8e2c2f87082be1a314dc
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-04-13 00:18:26 +0530

    Added JS to show alert message on form submission

--- JS/main.js
+++ JS/main.js
@@ -8,5 +8,16 @@ $(document).ready(function(){
     
     // Call the autoslider method for banner images
     autoSlides();
+
+    // Display success message on form submission
+    $("#my-form").submit(function(e) {
+        e.preventDefault();
+      
+        var $form = $(this);
+        $.post($form.attr("action"), $form.serialize()).then(function() {
+          alert("Thank you!");
+        });
+        return false;
+      });
 });
 
