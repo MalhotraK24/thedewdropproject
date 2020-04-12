897346d0ae1ca68d144804a77ca102b1b6db56b5
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-04-12 05:35:37 +0530

    Added the logic to show banner image if JS takes time to load. Added header content sitting on top of banner images. Lots of minor css changes to beautify the layout.

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     // Call the lightbox pluglin 
     $('.gallery a').simpleLightbox();
 
+    //Hide the default banner image before executing the slider
+    $('div.default').hide();
+    
     // Call the autoslider method for banner images
     autoSlides();
 });
