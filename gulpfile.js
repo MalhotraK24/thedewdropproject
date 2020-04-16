7c0aa58d7c4aa731d6dd3c0c2ffca5c072af2e20
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-04-17 03:01:45 +0530

    Updated gulpfile.js to remove commented code

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,5 @@ var imagemin = require('gulp-imagemin');
 exports.default = () => (
 	gulp.src('./Images/lets-talk.jpg')
 		.pipe(imagemin())
-        .pipe(gulp.dest('Gallery'))
+		.pipe(gulp.dest('Gallery'))
 );
-
-// exports.default = () => (
-// 	gulp.src('./Images/favicon_io/*')
-// 		.pipe(imagemin())
-//         .pipe(gulp.dest('Gallery/Icons/favicon_io'))
-// );
