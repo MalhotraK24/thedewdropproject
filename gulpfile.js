608250aa80506961494c409747007327cbda67ea
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-04-18 02:24:25 +0530

    Recompressed the Images using a different imagemin plugin specific for JEG and PNG images as WebP is not supported by Safari.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
-var imagemin = require('imagemin');
+const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
+const imageminOptipng = require('imagemin-optipng');
+const imageminJpegtran = require('imagemin-jpegtran');
 
-// Old method to convert/minimize images
+// Old method to compress images
 //	exports.default = () => (
 // 	gulp.src('./Old Gallery/*.{jpg,png}')
 // 		.pipe(imagemin(['./Old Gallery/*.{jpg,png}'], './Gallery', {
@@ -13,13 +15,25 @@ const imageminWebp = require('imagemin-webp');
 // 		.pipe(gulp.dest('./Gallery'))
 // );
 
-// New method to convert/minimize images using the quality as a metric
+// Method to compress jgp or jpeg images
+// exports.default = async() => {
+// 	const files = await imagemin(
+// 		['Images/Pratishtha/*.{jpg,jpeg}'],
+// 		{
+// 		  destination: 'Gallery/Pratishtha',
+// 		  plugins: [imageminJpegtran()]
+// 		}
+// 	);
+// 	console.log(files);
+//   };
+
+// Method to compress png images
 exports.default = async() => {
 	const files = await imagemin(
-		['Images/*.{jpg,jpeg,png,ico}'],
+		['Images/Icons/favicon_io/*.png'],
 		{
-		  destination: 'Gallery',
-		  plugins: [imageminWebp({quality: 50})]
+		  destination: 'Gallery/Icons/favicon_io',
+		  plugins: [imageminOptipng()]
 		}
 	);
 	console.log(files);
