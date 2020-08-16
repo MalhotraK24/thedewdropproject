84cace09828605d68ad3b25b2a2e17d8395af065
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-08-17 04:44:33 +0530

    Added a new Navigation to the app. Restructured the application and updated gulp file to pre-render nunjucks to html files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,79 @@
-var gulp = require('gulp');
-const imagemin = require('imagemin');
-const imageminWebp = require('imagemin-webp');
-const imageminOptipng = require('imagemin-optipng');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
+var gulp = require("gulp");
+var nunjucksRender = require("gulp-nunjucks-render");
 
-const PNGImages = 'Images/Icons/*.png';
-const JPEGImages = 'Images/*.{jpg,jpeg}';
-const output = 'Gallery';
+const imagemin = require("imagemin");
+const imageminWebp = require("imagemin-webp");
+const imageminOptipng = require("imagemin-optipng");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
 
-exports.default = () =>
- imagemin([JPEGImages], {
-	destination: output,
-    plugins: [ imageminMozjpeg({
-		quality: 50,
-		progressive: true,
-      }),
-    ]
+gulp.task("nunjucks", function () {
+  // Gets .html and .nunjucks files in pages
+  return (
+    gulp
+      .src("app/pages/**/*.+(html|njk)")
+      // Renders template with nunjucks
+      .pipe(
+        nunjucksRender({
+          path: ["app/templates"],
+        })
+      )
+      // output files in app folder
+      .pipe(gulp.dest("dist"))
+  );
+});
+
+// Minify CSS
+const cleanCSS = require("gulp-clean-css");
+
+gulp.task("minify-css", () => {
+  return gulp
+    .src("CSS/*.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("css-dist"));
+});
+
+// Compress PNGImages
+const PNGImages = "Images/Icons/*.png";
+const output = "Gallery/Icons";
+
+gulp.task("compress-png-images", () => {
+  imagemin([PNGImages], {
+    destination: output,
+    plugins: [imageminPngquant({ quality: [0.3, 0.5] })],
   })
-  .then(console.log("Compression successful!"))
-  .catch(error => console.log(error));
+    .then(console.log("Compression successful!"))
+    .catch((error) => console.log(error));
+});
 
-// exports.default = () =>
-//  imagemin([PNGImages], {
-//   destination: output,
-//   plugins: [
-// 	imageminPngquant({ quality: [0.3, 0.5] })
-//   ],
-// })
-// .then(console.log("Compression successful!"))
-// .catch(error => console.log(error));
+// Compress JPGImages
+const JPEGImages = "Images/Icons/{nbt|ht}-logo.{jpg,jpeg}";
+
+gulp.task("compress-jpg-images", () => {
+  imagemin([JPEGImages], {
+    destination: output,
+    plugins: [
+      imageminMozjpeg({
+        quality: 50,
+        progressive: true,
+      }),
+    ],
+  })
+    .then(console.log("Compression successful!"))
+    .catch((error) => console.log(error));
+});
+
+// Resize Images to 225x225
+const fs = require("fs");
+const resizeImg = require("resize-img");
+gulp.task("resize-images", () => {
+  const image = resizeImg(fs.readFileSync("Images/Icons/ht-logo.jpeg"), {
+    width: 225,
+    height: 225,
+  });
+  fs.writeFileSync("Images/Icons/ht-logo.jpeg", image);
+});
 
 // Old method to compress images
 // exports.default = () => (
@@ -42,27 +85,3 @@ exports.default = () =>
 // 		}))
 // 		.pipe(gulp.dest('./Gallery'))
 // );
-
-// Method to compress jgp or jpeg images
-// exports.default = async() => {
-// 	const files = await imagemin(
-// 		['Images/Pratishtha/*.jpg'],
-// 		{
-// 		  destination: 'Gallery/Pratishtha',
-// 		  plugins: [imageminJpegtran()]
-// 		}
-// 	);
-// 	console.log(files);
-//   };
-
-// Method to compress png images
-// exports.default = async() => {
-// 	const files = await imagemin(
-// 		['Images/Icons/favicon_io/*.png'],
-// 		{
-// 		  destination: 'Gallery/Icons/favicon_io',
-// 		  plugins: [imageminOptipng()]
-// 		}
-// 	);
-// 	console.log(files);
-//   };
