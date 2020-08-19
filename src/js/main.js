fbb12992c391499e584f289ded4db7a76f82e7b3
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-08-20 00:03:31 +0530

    Reverted the if condition to fire js plugin methods as the logic is incorect and breaking the slider and gallery functionality.

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,28 @@
 $(window).on("load", function () {
-  console.log(window.location.href);
-  if (window.location.href.endsWith("index.html")) {
-    // Call the autoslider method for banner images
-    autoSlides();
+  // Call the autoslider method for banner images
+  autoSlides();
 
-    // Call the lightbox pluglin
-    $(".gallery a").simpleLightbox();
+  // Call the lightbox pluglin
+  $(".gallery a").simpleLightbox();
 
-    // Display success message on form submission
-    $('form[name="contact-us"]').submit(function (e) {
-      e.preventDefault();
+  // Display success message on form submission
+  $('form[name="contact-us"]').submit(function (e) {
+    e.preventDefault();
 
-      // Putting the value from Name field to the hidden Subject field to create the subject of the email
-      var nameFieldValue = $('input[name="name"]').val();
-      $('input[name="subject"]').val(
-        "New quote request from : " + nameFieldValue
-      );
+    // Putting the value from Name field to the hidden Subject field to create the subject of the email
+    var nameFieldValue = $('input[name="name"]').val();
+    $('input[name="subject"]').val(
+      "New quote request from : " + nameFieldValue
+    );
 
-      // Display alert message on submission of the form
-      var $form = $(this);
-      $.post($form.attr("action"), $form.serialize()).then(function () {
-        showAlert();
-      });
-
-      // Clear all the fields or reset the form
-      $form[0].reset();
-      return false;
+    // Display alert message on submission of the form
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function () {
+      showAlert();
     });
-  }
+
+    // Clear all the fields or reset the form
+    $form[0].reset();
+    return false;
+  });
 });
