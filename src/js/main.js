691967516836a576246362f6febb56afe0e3e6f7
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-08-24 15:39:42 +0530

    Added new gif to be displayed while gallery images are loading. Updated index.html to not display gallery images till all the images are loaded which stops the functionality from breaking if the user clicks on any image. Updated main css to create a new class called hide.

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,36 @@ $(window).on("load", function () {
   // Call the autoslider method for banner images
   autoSlides();
 
-  // Call the lightbox pluglin
-  $(".gallery a").simpleLightbox();
+  // Hide the section displaying the gallery on load.
+  $("section.grid").attr("visibility", "hidden");
+  // Images loaded is zero because we're going to process a new set of images.
+  var imagesLoaded = 0;
+  // Total images is still the total number of <img> elements on the page.
+  var imgs = $("div.gallery img");
+  var totalImages = imgs.length;
+
+  // Step through each image in the DOM, clone it, attach an onload event
+  // listener, then set its source to the source of the original image. When
+  // that new image has loaded, fire the imageLoaded() callback.
+  imgs.each(function (idx, img) {
+    $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"));
+  });
+
+  // Do exactly as we had before -- increment the loaded count and if all are
+  // loaded, call the allImagesLoaded() function.
+  function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded == totalImages) {
+      allImagesLoaded();
+    }
+  }
+
+  function allImagesLoaded() {
+    console.log("ALL IMAGES LOADED");
+    $(".loading-images").addClass("hide");
+    $("section.grid").removeClass("hide");
+    $(".gallery a").simpleLightbox();
+  }
 
   // Display success message on form submission
   $('form[name="contact-us"]').submit(function (e) {
