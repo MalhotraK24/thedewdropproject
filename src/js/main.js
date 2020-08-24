dcbb3c8253d0a8216da09fc131ce1ab6d3433eb8
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-08-24 17:01:15 +0530

    Commented the iOS logic to diplay pop-up for PWA

--- src/js/main.js
+++ src/js/main.js
@@ -54,18 +54,18 @@ $(window).on("load", function () {
     return false;
   });
 
-  // Detects if device is on iOS
-  function isIos() {
-    var userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
-  }
-  // Detects if device is in standalone mode
-  function isInStandaloneMode() {
-    "standalone" in window.navigator && window.navigator.standalone;
-  }
+  // // Detects if device is on iOS
+  // function isIos() {
+  //   var userAgent = window.navigator.userAgent.toLowerCase();
+  //   return /iphone|ipad|ipod/.test(userAgent);
+  // }
+  // // Detects if device is in standalone mode
+  // function isInStandaloneMode() {
+  //   "standalone" in window.navigator && window.navigator.standalone;
+  // }
 
-  // Checks if should display install popup notification:
-  if (isIos() && !isInStandaloneMode()) {
-    this.setState({ showInstallMessage: true });
-  }
+  // // Checks if should display install popup notification:
+  // if (isIos() && !isInStandaloneMode()) {
+  //   this.setState({ showInstallMessage: true });
+  // }
 });
