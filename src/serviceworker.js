66b890fd526b0540bcc53a7b522bac65e8db4dd4
Kartik Malhotra <kartik@Kartiks-MacbookPro.local>
2020-08-28 01:59:13 +0530

    No comment.

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -14,7 +14,7 @@ self.addEventListener("install", function (event) {
 
 // This method is used to activate a new service worker when the old one is terminated
 self.addEventListener("activate", function (event) {
-  var cacheWhitelist = CACHE_NAME;
+  var cacheWhitelist = ["pages-cache-v2"];
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(
